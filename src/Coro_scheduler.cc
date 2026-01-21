ad2552aaad2639e2c33fc146dcf91f461b33ad17
red-shinku <3480198982@qq.com>
2026-01-21 11:49:10 +0800

    修改协程类初始与最终挂起行为，注册与注销

---
+++ src/Coro_scheduler.cc
@@ -0,0 +1,154 @@
+#include "Coro_scheduler.h"
+
+#include <iostream>
+#include <cppcoro/sync_wait.hpp>
+#include <spdlog/spdlog.h>
+
+using namespace easysv;
+
+Coro_scheduler::Coro_scheduler(EPOLL_EVENTS initial_care_event):
+epoll(), initial_care_event(initial_care_event), 
+fdlist(), coros{}, ending_queue(), ready_num(0)
+{
+    epoll.init();
+}
+
+Coro_scheduler::~Coro_scheduler()
+{
+    for(auto &coro: coros)
+    {
+        coro.second.coro_handle.destroy();
+    }
+}
+
+void Coro_scheduler::wait_event(int fd, handle_t coro_handle, EPOLL_EVENTS state)
+{
+    //如果状态变化修改epoll
+    try
+    {
+        if(coros.at(fd).state != state)
+        {
+            epoll.change_fd_event(fd, state);
+            coros.at(fd).state = state;
+        }
+    }
+    catch(const std::out_of_range& e)
+    {
+        spdlog::error("Coro_scheduler::wait_read: fd not found in coros map");
+        std::cerr << e.what() << '\n';
+    }
+    catch(const std::system_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+void Coro_scheduler::__register_coro__(int fd, handle_t coro_handle)
+{
+    try
+    {
+        if(coros.find(fd) == coros.end())
+        {
+            FdDetail fddetail{coro_handle, initial_care_event};
+            coros.emplace(fd, std::move(fddetail));
+            epoll.register_fd(fd, initial_care_event);
+        }
+        else
+            spdlog::warn("Coro_scheduler::register_wait_read: The FD {} has been register", fd);
+    }
+    catch(const std::bad_alloc& e)
+    {
+        spdlog::error("Coro_scheduler::register_wait_read: Register failed");
+        std::cerr << e.what() << '\n';
+    }
+    catch(const std::system_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+void Coro_scheduler::unregister_coro(int connfd, handle_t handle)
+{
+    ending_queue.push_back(handle);
+    if(coros.find(connfd) != coros.end())
+    {
+        try
+        {
+            epoll.deletefd(connfd);
+            coros.erase(connfd);
+            close(connfd); //FIXME: 处理close出错
+        }
+        catch(const std::system_error& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+    else
+    {
+        spdlog::warn("Delete an unexisting Fd in coros map");
+    }
+}
+
+void Coro_scheduler::destory_coro()
+{
+    for(auto& handle: ending_queue)
+    {
+        handle.destroy();
+    }
+    ending_queue.clear();
+}
+
+void Coro_scheduler::run()
+{
+    for(int i = 0; i < ready_num; ++i) {
+        int fd = fdlist[i].first;
+        auto it = coros.find(fd);
+        if (it == coros.end()) continue; //has been unregister
+
+        auto &fddetail = it->second;
+        if( fdlist[i].second & (fddetail.state | EPOLLHUP | EPOLLRDHUP | EPOLLERR) ) {
+            fddetail.coro_handle.resume();
+        }
+    }
+    destory_coro();
+}
+
+
+void Coro_scheduler::ready_next_run()
+{   //wait epoll
+    try
+    {
+        auto [rfdlist, rdynum] = epoll.wait(std::move(fdlist));
+        this->fdlist = rfdlist;
+        ready_num = rdynum;
+    }
+    catch(const std::system_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+void Coro_scheduler::register_coro(int connfd, callable_coro_t coro)
+{
+    try
+    {
+        if(coros.find(connfd) == coros.end())
+        {   //first run a coro and register
+            coro(
+                [this] -> Coro_scheduler& { return *this; } (),
+                connfd
+            ); 
+        }
+        else
+        {
+            spdlog::warn("Register an existing Fd in coros map");
+        }   
+    }
+    catch(const std::bad_alloc& e)
+    {
+        spdlog::error("out of memory: coros map");
+        std::cerr << e.what() << '\n';
+    }
+    
+}
+
---
+++ src/Coro_scheduler.h
@@ -0,0 +1,153 @@
+// -----------------------------------------------------------------------
+// this file define : task(coro type), Coro_scheduler and two Awaitable --
+// AwaitReadable and AwaitWriteable
+// 
+// the coroutine sheduler is for one thread.
+// It also responsible for the management of coro(save a list of handles).
+// -----------------------------------------------------------------------
+#pragma once
+
+#include <cstdint>
+#include <unordered_map>
+#include <functional>
+#include <coroutine>
+#include <cppcoro/task.hpp>
+#include "Epoll.h"
+
+namespace easysv 
+{
+
+struct AwaitReadable;
+struct AwaitWriteable;
+class Coro_scheduler;
+
+class task 
+{
+public:
+    ~task() { /*no destory handle*/ }
+    struct promise_type 
+    {
+        promise_type(Coro_scheduler& sched, int connfd):
+        sched(sched), connfd(connfd) { }
+
+        Coro_scheduler& sched;
+        int connfd;
+        
+        //when first run a coro, suspend at first and register it
+        auto initial_suspend() noexcept
+        {
+            struct AwaitFirst
+            {
+                bool await_ready() const noexcept { return false; }
+                void await_suspend(std::coroutine_handle<promise_type> h)
+                {
+                    h.promise().sched.__register_coro__(
+                        h.promise().connfd,
+                        h
+                    );
+                }
+                void await_resume() const noexcept {}
+            };
+            return AwaitFirst{};
+        }
+
+        //when co_return, put the handle in ending_queue
+        auto final_suspend() noexcept
+        { 
+            struct AwaitFinal
+            {
+                bool await_ready() const noexcept { return false; }
+                void await_suspend(std::coroutine_handle<promise_type> h)
+                {
+                    h.promise().sched.unregister_coro(
+                        h.promise().connfd,
+                        h
+                    );
+                }
+                void await_resume() const noexcept {}
+            };
+            return AwaitFinal{};
+        }
+
+        task get_return_object() 
+        { 
+            return task{ std::coroutine_handle<promise_type>::from_promise(*this) }; 
+        }
+
+        void unhandled_exception() { std::terminate(); }
+        
+        void return_void() {}
+    };
+
+    std::coroutine_handle<promise_type> handle;
+
+};
+
+using coro_t = task;
+using handle_t = std::coroutine_handle<task::promise_type>;
+using callable_coro_t = std::function<coro_t(Coro_scheduler&, int/*fd*/)>;
+
+typedef struct FdDetail
+{
+    //coros to be sheduler
+    handle_t coro_handle;
+    uint32_t state;
+}FdDetail;
+
+
+class Coro_scheduler
+{
+    friend struct Awaitable;
+    friend struct task;
+
+private:
+    Epoll epoll;
+    EPOLL_EVENTS initial_care_event;
+    Epoll::fdarray_t fdlist; //{fd, care-event}
+    std::unordered_map<int, FdDetail> coros;
+    std::vector<handle_t> ending_queue;
+    int ready_num;
+
+    //use in initial_suspend
+    void __register_coro__(int fd, handle_t);
+    //use in final_suspend, move the handle to ending queue
+    void unregister_coro(int connfd, handle_t);
+    //take handles in ending queue and destory it
+    void destory_coro();
+
+    //method for coro to call in Await, when it suspend
+    void wait_event(int fd, handle_t, EPOLL_EVENTS state);
+
+public:
+    explicit Coro_scheduler(EPOLL_EVENTS initial_care_event);
+    ~Coro_scheduler() noexcept;
+    Coro_scheduler(const Coro_scheduler&) = delete;
+    Coro_scheduler& operator=(const Coro_scheduler&) = delete;
+    
+    Coro_scheduler(Coro_scheduler&&) = default;
+    Coro_scheduler& operator=(Coro_scheduler&&) = default;
+
+    //the thread's main coro
+    void run();
+    void ready_next_run();
+
+    //work thread call it to register coro
+    void register_coro(int connfd, callable_coro_t coro);
+};
+
+struct Awaitable
+{
+    Awaitable(Coro_scheduler& sched, int fd, EPOLL_EVENTS care_event):
+    sched(sched), fd(fd), care_event(care_event) { }
+
+    Coro_scheduler& sched;
+    int fd;
+    EPOLL_EVENTS care_event;
+
+    bool await_ready() const noexcept { return false; }
+    void await_suspend(handle_t coro_han)
+    { sched.wait_event(fd, coro_han, care_event); }
+    void await_resume() noexcept { }
+};
+
+}

9a934856aba7b89060c8d9cfb17a32f7817a4d43
red-shinku <3480198982@qq.com>
2026-01-27 13:27:29 +0800

    重载wait无参版本，删除移动操作

--- src/Epoll.cc
+++ src/Epoll.cc
@@ -2,8 +2,7 @@
 #include <errno.h>
 #include <stdexcept>
 #include <spdlog/spdlog.h>
-
-#define EPOLLMOD EPOLLET
+#include "../include/config.h"
 
 using namespace easysv;
 
@@ -23,13 +22,14 @@ void Epoll::init()
         spdlog::error("failed to create epoll");  
         throw std::runtime_error("epoll_create1 error");
     }
+    events.resize(g_config.EventArraySize);
 }
 
-void Epoll::register_fd(int connfd, EPOLL_EVENTS care_event)
+void Epoll::register_fd(int fd, EPOLL_EVENTS care_event)
 {
-    ev_sample.data.fd = connfd;
-    ev_sample.events = care_event | EPOLLMOD;
-    if(epoll_ctl(epfd, EPOLL_CTL_ADD, connfd, &ev_sample) == -1)
+    ev_sample.data.fd = fd;
+    ev_sample.events = care_event | g_config.EPOLLMOD;
+    if(epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev_sample) == -1)
     {
         spdlog::error("failed to add epoll");
         throw std::system_error(
@@ -40,11 +40,11 @@ void Epoll::register_fd(int connfd, EPOLL_EVENTS care_event)
     }
 }
 
-void Epoll::change_fd_event(int connfd, EPOLL_EVENTS care_event)
+void Epoll::change_fd_event(int fd, EPOLL_EVENTS care_event)
 {
-    ev_sample.data.fd = connfd;
-    ev_sample.events = care_event | EPOLLMOD;
-    if(epoll_ctl(epfd, EPOLL_CTL_MOD, connfd, &ev_sample) == -1)
+    ev_sample.data.fd = fd;
+    ev_sample.events = care_event | g_config.EPOLLMOD;
+    if(epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &ev_sample) == -1)
     {
         spdlog::error("failed to change epoll event");
         throw std::system_error(
@@ -55,9 +55,9 @@ void Epoll::change_fd_event(int connfd, EPOLL_EVENTS care_event)
     }
 }
 
-void Epoll::deletefd(int connfd)
+void Epoll::deletefd(int fd)
 {
-    if(epoll_ctl(epfd, EPOLL_CTL_DEL, connfd, NULL) == -1)
+    if(epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL) == -1)
     {
         spdlog::error("failed to del epoll");
         throw std::system_error(
@@ -72,7 +72,7 @@ std::tuple<Epoll::fdarray_t, int> Epoll::wait(fdarray_t&& fdlist)
 {
     int n = 0;
     //FIXME: epoll wait 返回EINTR时重试
-    if((n = epoll_wait(epfd, events, Epoll::EventArraySize, 500)) == -1)
+    if((n = epoll_wait(epfd, events.data(), g_config.EventArraySize, -1)) == -1)
     {
         spdlog::error("failed from epoll_wait()");
         throw std::system_error(
@@ -87,6 +87,12 @@ std::tuple<Epoll::fdarray_t, int> Epoll::wait(fdarray_t&& fdlist)
         fdlist[i].second = events[i].events;
     }
     return {std::move(fdlist), n};
+}
+
+std::tuple<Epoll::fdarray_t, int> Epoll::wait()
+{
+    Epoll::fdarray_t fds(g_config.EventArraySize);
+    return wait(std::move(fds));
 }
 
     
--- src/Epoll.h
+++ src/Epoll.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <cstdint>
-#include <array>
+#include <vector>
 #include <tuple>
 #include <sys/epoll.h>
 
@@ -10,10 +10,10 @@ namespace easysv
 class Epoll
 {
 private:
-    static constexpr int EventArraySize = 50;
+    // static constexpr int EventArraySize = 50;
     using event_t = struct epoll_event;
     event_t ev_sample;
-    event_t events[EventArraySize];
+    std::vector<event_t> events;
     int epfd;
 
 public:
@@ -22,20 +22,22 @@ public:
     Epoll(const Epoll&) = delete;
     Epoll& operator=(const Epoll&) = delete;
     
-    Epoll(Epoll&&) = default;
-    Epoll& operator=(Epoll&&) = default;
+    Epoll(Epoll&&) = delete;
+    Epoll& operator=(Epoll&&) = delete;
 
     //this array create by outside, move in to handle
-    using fdarray_t = std::array<std::pair<int, uint32_t>, EventArraySize>;
+    using fdarray_t = std::vector<std::pair<int, uint32_t>>;
 
     //epoll_create1
     void init();
     //epoll_ctl
-    void register_fd(int connfd, EPOLL_EVENTS care_event);
-    void change_fd_event(int connfd, EPOLL_EVENTS care_event);
-    void deletefd(int connfd);
+    void register_fd(int fd, EPOLL_EVENTS care_event);
+    void change_fd_event(int fd, EPOLL_EVENTS care_event);
+    void deletefd(int fd);
     //epoll_wait
     std::tuple<fdarray_t, int> wait(fdarray_t&& fdlist);
+    std::tuple<fdarray_t, int> wait();
+
 };
 
     

b9038be5d2850c460b36b82f133b664d5a6bb267
red-shinku <3480198982@qq.com>
2026-02-13 13:27:19 +0800

    更改用户设置选项

--- include/config.h
+++ include/config.h
@@ -4,14 +4,10 @@
 
 struct Setting
 {
-    //each time thread get nums of fd from pub_queue
-    size_t EACH_FD_GET_NUM;
-    //size of pub fd queue
-    size_t PUB_FD_QUEUE_SIZE;
-    //the nums when pub fd queue turn to busy state 
-    size_t PUB_FD_QUEUE_CRITICAL;
-    //the nums when thread turn to busy state
-    size_t IS_IDLE_NUM;
+    //the size of listen sock queue
+    int LISTENQ;
+    //each time thread get nums of fd when accept
+    size_t EACH_ACCEPT_NUM;
     //the max nums of fd epoll wait return
     size_t EventArraySize;
     //epoll mod -- LT or ET
